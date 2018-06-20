a9d5c8694d2c476186d88d3f5fcdb7e90bf56d5b
bit <kickscar@gmail.com>
2018-06-20 11:44:23 +0900

    crawling_pelicana 함수 구현1

--- __main__.py
+++ __main__.py
@@ -1,9 +1,36 @@
+from itertools import count
+from bs4 import BeautifulSoup
 import collection.crawler as cw
 
 
+def crawling_pelicana():
+    results = []
+    for page in range(1, 2):
+        url = 'http://www.pelicana.co.kr/store/stroe_search.html?gu=&si=&page=%d' % page
+        html = cw.crawling(url=url)
 
-cw.crawling(
-    url='http://movie.naver.com/movie/sdb/rank/rmovie.nhn',
-    encoding='cp949')
+        bs = BeautifulSoup(html, 'html.parser')
+        tag_table = bs.find('table', attrs={'class': 'table mt20'})
+        tag_tbody = tag_table.find('tbody')
+        tags_tr = tag_tbody.findAll('tr')
+
+        # 끝 검출
+        if len(tags_tr) == 0:
+            break
+
+        for tag_tr in tags_tr:
+            strings = list(tag_tr.strings)
+
+            name = strings[1]
+            address = strings[3]
+            sidogu = address.split()[:2]
+
+            results.append((name, address) + tuple(sidogu))
+    #proc
+    print(results)
+
+if __name__ == '__main__':
+    # pelicana
+    crawling_pelicana()
 
 
