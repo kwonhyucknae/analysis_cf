8f989a4aedaf1408116ab54979d3d30495c86bf0
bit <kickscar@gmail.com>
2018-06-20 13:50:52 +0900

    nene 구현 완료

--- __main__.py
+++ __main__.py
@@ -1,6 +1,12 @@
+import urllib
 from itertools import count
+import pandas as pd
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree as et
 import collection.crawler as cw
+from collection.data_dict import sido_dict, gungu_dict
+
+RESULT_DIRECTORY = '__result__/crawling'
 
 
 def crawling_pelicana():
@@ -26,11 +32,49 @@ def crawling_pelicana():
             sidogu = address.split()[:2]
 
             results.append((name, address) + tuple(sidogu))
-    #proc
-    print(results)
+
+    # store
+    table = pd.DataFrame(results, columns=['name', 'address', 'sido', 'gungu'])
+
+    table['sido'] = table.sido.apply(lambda v: sido_dict.get(v, v))
+    table['gungu'] = table.gungu.apply(lambda v: gungu_dict.get(v, v))
+
+    table.to_csv('{0}/pelicana_table.csv'.format(RESULT_DIRECTORY), encoding='utf-8', mode='w', index=True)
+
+
+def proc_nene(xml):
+    root = et.fromstring(xml)
+    results = []
+
+    for el in root.findall('item'):
+        name = el.findtext('aname1')
+        sido = el.findtext('aname2')
+        gungu = el.findtext('aname3')
+        address = el.findtext('aname5')
+
+        results.append((name, address, sido, gungu))
+
+    return results
+
+
+def store_nene(data):
+    table = pd.DataFrame(data, columns=['name', 'address', 'sido', 'gungu'])
+
+    table['sido'] = table.sido.apply(lambda v: sido_dict.get(v, v))
+    table['gungu'] = table.gungu.apply(lambda v: gungu_dict.get(v, v))
+
+    table.to_csv('{0}/nene_table.csv'.format(RESULT_DIRECTORY), encoding='utf-8', mode='w', index=True)
+
+
 
 if __name__ == '__main__':
     # pelicana
     crawling_pelicana()
 
+    # nene
+    cw.crawling(
+        url='http://nenechicken.com/subpage/where_list.asp?target_step2=%s&proc_type=step1&target_step1=%s'
+            % (urllib.parse.quote("전체"), urllib.parse.quote("전체")),
+        proc=proc_nene,
+        store=store_nene)
 
