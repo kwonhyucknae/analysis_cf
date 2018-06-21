67756933f1b85b73f3c9b9f3e069d953f1bd9994
bit <kickscar@gmail.com>
2018-06-21 10:47:25 +0900

    crawling_nene 구현 1

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@ from itertools import count
 import pandas as pd
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as et
+import time
+from selenium import webdriver
 import collection.crawler as cw
 from collection.data_dict import sido_dict, gungu_dict
 
@@ -66,15 +68,56 @@ def store_nene(data):
     table.to_csv('{0}/nene_table.csv'.format(RESULT_DIRECTORY), encoding='utf-8', mode='w', index=True)
 
 
+def crawling_kyochon():
+    pass
+
+
+def crawling_goobne():
+    url = 'http://www.goobne.co.kr/store/search_store.jsp'
+
+    # 첫 페이지 로딩
+    wd = webdriver.Chrome('D:/bigdata/chromedriver/chromedriver.exe')
+    wd.get(url)
+    time.sleep(5)
+
+    for page in range(101, 105):
+        # 자바스크립트 실행
+        script = 'store.getList(%d)' % page
+        wd.execute_script(script)
+        time.sleep(5)
+
+        # 실행결과HTML(rendering된 HTML) 가져오기
+        html = wd.page_source
+
+        # parsing with bs4
+        bs = BeautifulSoup(html, 'html.parser')
+        tag_tbody = bs.find('tbody', attrs={'id': 'store_list'})
+        tags_tr = tag_tbody.findAll('tr')
+
+        # 마지막 검출
+        if tags_tr[0].get('class') is None:
+            break
+
+        print(tag_tbody)
+
+
+
 
 if __name__ == '__main__':
     # pelicana
-    crawling_pelicana()
+    # crawling_pelicana()
 
     # nene
+    '''
     cw.crawling(
         url='http://nenechicken.com/subpage/where_list.asp?target_step2=%s&proc_type=step1&target_step1=%s'
             % (urllib.parse.quote("전체"), urllib.parse.quote("전체")),
         proc=proc_nene,
         store=store_nene)
+    '''
 
+    # kyochon
+    # crawling_kyochon()
+
+    # goobne
+    crawling_goobne()
